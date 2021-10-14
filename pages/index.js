f79e327d25f99af54305d6c99297ea7cb28f40f7
Hlib Koshukov <hlib@24.design>
2021-10-14 22:43:16 +0300

    meta v1.0 && api update && stettings context working properly

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,86 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import styles from '../styles/Home.module.css';
-import { getPosts } from '../lib/api';
+// import styles from '../styles/Home.module.css';
+import { getPosts, getSettings } from '../lib/api';
 
-export default function IndexPage({posts}){
+export default function Index({settings}){
   return(
-    <ul>
-    {posts.map(post => (
-      <li key={post.id}>
-        <Link href={`post/[slug].js`} as={`post/${post.slug}`}>
-          <Image
-            src = {post.feature_image}
-            alt = {post.feature_image_alt}
-            width = {500}
-            height = {320}
-          />
-        </Link>
-        <div></div>
-        <Link href={`tag/[slug].js`} as={`tag/${post.primary_tag.slug}`}>
-          <a>{post.primary_tag.name}</a>
-        </Link>
-        <div></div>
-        <Link href={`post/[slug].js`} as={`post/${post.slug}`}>
-          <a>{post.title}</a>
-        </Link>
-        <div>
-          <p>{post.created_at} - Time to read {post.reading_time} minutes</p>
-        </div>
-      </li>
-    ))}
-  </ul>
-      );
+    <div>
+        <h1>Title: {settings.title}</h1>
+        <p>Description: {settings.description}</p>
+        <p>Navigation:</p>
+        <ul>
+          {settings.navigation.map(nav => (
+            <li key={nav.url}>
+              <p>label:{nav.label}, url:{nav.url}</p>
+            </li>
+          ))}
+        </ul>
+        <p>Secondary navigation:</p>
+        <ul>
+          {settings.secondary_navigation.map(nav => (
+            <li key={nav.url}>
+              <p>label:{nav.label}, url:{nav.url}</p>
+            </li>
+          ))}
+        </ul>
+    </div>
+  )
 }
-export async function getStaticProps(context) {
-  const posts = await getPosts()
+
+export async function getStaticProps(context){
+  const settings = await getSettings();
   
-  if (!posts) {
-    return {
+  if (!settings){
+    return{
       notFound: true,
     }
   }
 
-  return {
-    props: { posts }
+  return{
+    props:{settings}
   }
 }
+
+// export default function IndexPage({posts}){
+//   return(
+//     <ul>
+//     {posts.map(post => (
+//       <li key={post.id}>
+//         <Link href={`post/[slug].js`} as={`post/${post.slug}`}>
+//           <Image
+//             src = {post.feature_image}
+//             alt = {post.feature_image_alt}
+//             width = {500}
+//             height = {320}
+//           />
+//         </Link>
+//         <div></div>
+//         <Link href={`tag/[slug].js`} as={`tag/${post.primary_tag.slug}`}>
+//           <a>{post.primary_tag.name}</a>
+//         </Link>
+//         <div></div>
+//         <Link href={`post/[slug].js`} as={`post/${post.slug}`}>
+//           <a>{post.title}</a>
+//         </Link>
+//         <div>
+//           <p>{post.created_at} - Time to read {post.reading_time} minutes</p>
+//         </div>
+//       </li>
+//     ))}
+//   </ul>
+//       );
+// }
+// export async function getStaticProps(context) {
+//   const posts = await getPosts()
+  
+//   if (!posts) {
+//     return {
+//       notFound: true,
+//     }
+//   }
+
+//   return {
+//     props: { posts }
+//   }
+// }
