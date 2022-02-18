78966605e93ab819f64ed50f6255a987c10afd34
Hlib Koshukov <hlib@24.design>
2022-02-18 13:39:58 +0200

    revisited project; desided to start from here and move with Next.js for the front-end

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@ module.exports = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   images: {
-    domains: ['164.90.237.14', 'static.ghost.org'],
+    domains: ['https://dzherelo.one'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
+  },
+  i18n: {
+    locales: ['uk', 'en'],
+    defaultLocale: 'uk',
+    localeDetection: false,
   },
   env:{}
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend:{
+      colors:{
+        white: colors.white,
+        'brand-green': '#00FFCC',
+        'brand-background': '#161616',
+        'highlight-grey': '#8BA3BA',
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-nested')
+  ],
+};
